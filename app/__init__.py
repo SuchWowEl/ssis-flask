11be0e85b411424cc7bf1c872e6764cdaebc6cfa
Elizer Dolorosa <43069251+SuchWowEl@users.noreply.github.com>
2023-10-13 03:13:12 +0800

    refactor: remove bootstrap in init

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 print('eyo')
 from flask import Flask, render_template, jsonify, request
 from flask_mysql_connector import MySQL
-from flask_bootstrap import Bootstrap
+# from flask_bootstrap import Bootstrap
 from config import DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, SECRET_KEY, BOOTSTRAP_SERVE_LOCAL
 from flask_wtf.csrf import CSRFProtect
 
 mysql = MySQL()
-bootstrap = Bootstrap()
+# bootstrap = Bootstrap()
 
 def create_app(test_config=None):
     app = Flask(__name__, static_folder='static', static_url_path='/static', instance_relative_config=True)
@@ -18,7 +18,7 @@ def create_app(test_config=None):
         MYSQL_HOST=DB_HOST,
         #BOOTSTRAP_SERVE_LOCAL=BOOTSTRAP_SERVE_LOCAL
     )
-    bootstrap.init_app(app)
+    # bootstrap.init_app(app)
     mysql.init_app(app)
     CSRFProtect(app)
 
