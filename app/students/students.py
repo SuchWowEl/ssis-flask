19685dd0449ee4435e7f38e54df023af5a73fb3c
Elizer Dolorosa <43069251+SuchWowEl@users.noreply.github.com>
2023-10-18 15:43:52 +0800

    feat: ADD&EDIT for student
    note: converted some/all ajax code snippets to javascript
          XMLHttpRequest not secure
    todo: validate

--- app/students/students.py
+++ app/students/students.py
@@ -6,16 +6,61 @@ from werkzeug.exceptions import abort
 # from flaskr.auth import login_required
 # from flaskr.db import get_db
 
-from app.models import student_interface
+from app.models import student_interface, ofcourse
 
 bp = Blueprint('students', __name__)
 
-@bp.route('/students/', methods=['GET', 'POST'])
+studentid = ""
+
+@bp.route('/students/')
 def students():
-    if request.method == 'GET':
-        student_table = student_interface.all()
-        student_table.insert(0, student_interface.headers())
-        print("students retrieved")
-        # student_table = json.loads(student_table)
-        return render_template("student/student.html", content=student_table)
-        # return jsonify({'students': student_table})
+    global studentid
+    studentid = ""
+    student_table = student_interface.all()
+    student_table.insert(0, student_interface.headers())
+    print("students retrieved")
+    return render_template("student/student.html", content=student_table)
+    
+@bp.route('/students/add/')
+def students_add_view():
+    course_table = ofcourse.coursecodes()
+    print(course_table)
+    courses = [item[0] for item in course_table]
+    return render_template("student/edit.html", content=courses)
+    
+@bp.route('/students/addstudent/', methods=['POST'])
+def students_add():
+    print("students_add called")
+    data = request.get_json()
+    print("form: " + str(data))
+    student_interface.add(data)
+    return redirect(url_for("students.students"))
+    
+@bp.route('/students/edit/', methods=['POST'])
+def students_edit_view():
+    print("students_add called")
+    data = request.get_json()
+    studentInfo = student_interface.retrieve_student(data["id"])
+    global studentid 
+    studentid = data["id"]
+    course_table = ofcourse.coursecodes()
+    print("studentInfo from controller:")
+    print(studentInfo)
+    print(course_table)
+    courses = [item[0] for item in course_table]
+    return render_template("student/edit.html", content=courses, info=studentInfo)
+    
+@bp.route('/students/editstudent/', methods=['POST'])
+def students_edit():
+    global studentid 
+    print("students_edit called")
+    data = request.get_json()
+    print("form: " + str(data))
+    student_interface.update(data, studentid)
+    studentid = ""
+    return redirect(url_for("students.students"))
+    # student_table = student_interface.all()
+    # student_table.insert(0, student_interface.headers())
+    # print("students retrieved")
+    # return render_template("student/student.html", content=student_table)
+        # return jsonify({'courses': course_table})
