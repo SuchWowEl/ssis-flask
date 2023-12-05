6c171321b4c230418df62fb423e84b42e1a99d03
Elizer Dolorosa <zykisriel0434@gmail.com>
2023-12-05 14:33:00 +0800

    fix: set default pic if no uploaded pic

--- app/students/students.py
+++ app/students/students.py
@@ -38,11 +38,11 @@ def students_add():
     #print("form: " + str(data))
     try:
         pic_link = data['picture']
-        if pic_link:
+        if pic_link != "static/default_pic.svg":
             upload_result = upload(pic_link, folder="SSIS", resource_type='image')
             secure_url = upload_result['secure_url']
         else:
-            secure_url = None
+            secure_url = ""
         data['picture'] = secure_url
         print(f"secure_url: {secure_url}")
         student_interface.add(data)
